d7aea747df4824397b1fe167b162d5ddc18877d9
kavindu919 <jayakodyk2@gmail.com>
2025-03-22 01:26:45 +0530

    fix chnages

--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -1,11 +1,16 @@
-import express from 'express';
-import * as bookingController from '../controllers/bookingController.js';
+import express from "express";
+import {
+  cancelBooking,
+  createBooking,
+  getAllBookings,
+  getUserBookings,
+} from "../controllers/bookingController.js";
 const router = express.Router();
 
-router.post('/bookings', bookingController.createBooking);
-router.get('/bookings/user/:userId', bookingController.getUserBookings);
-router.get('/bookings', bookingController.getAllBookings);
-router.put('/bookings/:id/cancel', bookingController.cancelBooking);
-router.put('/bookings/:id/status', bookingController.updateBookingStatus);
+router.post("/bookings", createBooking);
+router.get("/bookings/user/:userId", getUserBookings);
+router.get("/bookings", getAllBookings);
+router.post("/bookings/:id/cancel", cancelBooking);
+// router.post("/bookings/:id/status", updateBookingStatus);
 
 export default router;
